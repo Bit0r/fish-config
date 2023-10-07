3f075ee087339e611d1835200b52db4b30ef11cc
Bit0r <nie_wang@outlook.com>
2023-10-07 23:27:16 +0800

    📦 add DengXian font and simplify the code

--- fonts.fish
+++ fonts.fish
@@ -1,14 +1,17 @@
 #!/usr/bin/fish
 
-sudo mkdir -p /usr/local/share/fonts/{sim,wingdings,fancy,gb2312,YaHei}/
+sudo chmod 1777 /usr/local/share/fonts
 
-sudo cp fonts/Sim*.ttf /usr/local/share/fonts/sim/
-sudo cp fonts/Wingdings*.ttf /usr/local/share/fonts/wingdings/
-sudo cp fonts/{MTExtra, Symbol}.ttf /usr/local/share/fonts/fancy/
-sudo cp fonts/*_GB2312.ttf /usr/local/share/fonts/gb2312/
-sudo cp fonts/MicrosoftYaHei*.ttf /usr/local/share/fonts/YaHei/
+mkdir -p /usr/local/share/fonts/{sim,wingdings,fancy,gb2312,YaHei,Deng}/
 
-fc-cache -f -v
+cp fonts/Sim*.ttf /usr/local/share/fonts/sim/
+cp fonts/Wingdings*.ttf /usr/local/share/fonts/wingdings/
+cp fonts/{MTExtra, Symbol}.ttf /usr/local/share/fonts/fancy/
+cp fonts/*_GB2312.ttf /usr/local/share/fonts/gb2312/
+cp fonts/MicrosoftYaHei*.ttf /usr/local/share/fonts/YaHei/
+cp fonts/Deng*.ttf /usr/local/share/fonts/Deng/
+
+fc-cache -fv
 
 if type -q mplfonts
     sudo mplfonts init
