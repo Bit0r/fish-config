15a93c49a7619b6cb3b826dd66db2d139b710fd1
Bit0r <nie_wang@outlook.com>
2023-10-08 23:20:09 +0800

    🎨 add some comments

--- sources.fish
+++ sources.fish
@@ -14,10 +14,18 @@ end
 # 添加外部源
 if confirm 'Do you want to add other software sources?'
     sudo apt install curl
+
+    # nodejs
     curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+
+    # docker
     curl -fsSL http://mirrors.163.com/docker-ce/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker-ce-163.gpg >/dev/null
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker-ce-163.gpg] https://mirrors.163.com/docker-ce/linux/ubuntu $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker-ce-163.list >/dev/null
+
+    # wine
     sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
     sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/$UBUNTU_CODENAME/winehq-$UBUNTU_CODENAME.sources"
+
+    # unit
     #sudo cp config/unit.list /etc/apt/sources.list.d/
 end
