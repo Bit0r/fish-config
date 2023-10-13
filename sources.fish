2be793315af1aaa4585bbd94b8c619ec5d4ce60b
Bit0r <nie_wang@outlook.com>
2023-10-13 19:29:12 +0800

    🎨 Add quotation marks to the URL

--- sources.fish
+++ sources.fish
@@ -16,19 +16,19 @@ if confirm 'Do you want to add other software sources?'
     sudo apt install curl
 
     # nodejs
-    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+    curl -fsSL 'https://deb.nodesource.com/setup_lts.x' | sudo -E bash -
 
     # docker
-    curl -fsSL http://mirrors.163.com/docker-ce/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker-ce-163.gpg >/dev/null
+    curl -fsSL 'http://mirrors.163.com/docker-ce/linux/ubuntu/gpg' | gpg --dearmor | sudo tee /etc/apt/keyrings/docker-ce-163.gpg >/dev/null
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker-ce-163.gpg] https://mirrors.163.com/docker-ce/linux/ubuntu $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker-ce-163.list >/dev/null
 
     # wine
-    sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+    sudo wget -O /etc/apt/keyrings/winehq-archive.key 'https://dl.winehq.org/wine-builds/winehq.key'
     #sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/$UBUNTU_CODENAME/winehq-$UBUNTU_CODENAME.sources"
     echo "deb [arch=amd64,i386 signed-by=/etc/apt/keyrings/winehq-archive.key] http://mirrors.cernet.edu.cn/wine-builds/ubuntu/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/winehq.list >/dev/null
 
     # zotero
-    proxy-aria2c -d /tmp/ https://raw.githubusercontent.com/retorquere/zotero-deb/master/zotero-archive-keyring.gpg
+    proxy-aria2c -d /tmp/ 'https://raw.githubusercontent.com/retorquere/zotero-deb/master/zotero-archive-keyring.gpg'
     sudo mv /tmp/zotero-archive-keyring.gpg /usr/share/keyrings/
     echo 'deb [signed-by=/usr/share/keyrings/zotero-archive-keyring.gpg by-hash=force] https://zotero.retorque.re/file/apt-package-archive ./' | sudo tee /etc/apt/sources.list.d/zotero.list >/dev/null
 
