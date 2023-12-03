0c39d6c51dda8f02929d9b75cb0ff7d062d40751
Bit0r <nie_wang@outlook.com>
2023-12-03 14:03:08 +0800

    Refactor the third-party sources code.

--- sources.fish
+++ sources.fish
@@ -15,28 +15,21 @@ end
 if confirm 'Do you want to add some third-party apt sources?'
     sudo apt install curl
 
-    # nodejs
-    curl -fsSL 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key' | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    set -l keyrings_dir /etc/apt/keyrings
+    sudo mkdir -p $keyrings_dir
+
     set -l NODE_MAJOR 20
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list >/dev/null
+    for source_conf in (cat ./config/apt/sources.tsv | grep -v '^#')
+        set source_conf (string split \t $source_conf)
+        set -l source_name $source_conf[1]
+        set -l key_url $source_conf[2]
+        set -l source_url $source_conf[3]
 
-    # HashiCorp
-    #proxy-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-    #echo "deb [arch=amd64 signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+        set -l key_path $keyrings_dir/$source_name.gpg
 
-    # docker
-    curl -fsSL 'http://mirrors.163.com/docker-ce/linux/ubuntu/gpg' | sudo gpg --dearmor -o /etc/apt/keyrings/docker-ce-163.gpg
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker-ce-163.gpg] https://mirrors.163.com/docker-ce/linux/ubuntu $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker-ce-163.list >/dev/null
-
-    # wine
-    sudo wget -O /etc/apt/keyrings/winehq-archive.key 'https://dl.winehq.org/wine-builds/winehq.key'
-    #sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/$UBUNTU_CODENAME/winehq-$UBUNTU_CODENAME.sources"
-    echo "deb [arch=amd64,i386 signed-by=/etc/apt/keyrings/winehq-archive.key] http://mirrors.cernet.edu.cn/wine-builds/ubuntu/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/winehq.list >/dev/null
-
-    # zotero
-    proxy-aria2c -d /tmp/ 'https://raw.githubusercontent.com/retorquere/zotero-deb/master/zotero-archive-keyring.gpg'
-    sudo mv /tmp/zotero-archive-keyring.gpg /usr/share/keyrings/
-    echo 'deb [signed-by=/usr/share/keyrings/zotero-archive-keyring.gpg by-hash=force] https://zotero.retorque.re/file/apt-package-archive ./' | sudo tee /etc/apt/sources.list.d/zotero.list >/dev/null
+        proxy-curl -fsSL $key_url | sudo gpg --dearmor -o $key_path
+        echo "echo $source_url" | source | sudo tee /etc/apt/sources.list.d/$source_name.list >/dev/null
+    end
 
     # unit
     #sudo cp config/unit.list /etc/apt/sources.list.d/
