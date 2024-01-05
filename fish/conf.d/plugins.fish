b3310a6557ae1f75cd280a7de08b3f81c6c368c0
Bit0r <nie_wang@outlook.com>
2024-01-05 22:11:43 +0800

    feat: Add micro and Update fish plugin configs.

--- fish/conf.d/plugins.fish
+++ fish/conf.d/plugins.fish
@@ -2,7 +2,7 @@ if status is-interactive
     if type -q zoxide
         zoxide init fish | source
     end
-    if type -q atuin
-        atuin init fish | source
-    end
+    # if type -q atuin
+    #     atuin init fish | source
+    # end
 end
