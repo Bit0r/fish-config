0a8ff6bc5af52b7eb7d36d8ea2898b80d9a1e347
Bit0r <nie_wang@outlook.com>
2024-03-03 12:00:10 +0800

    feat: Add LiteLoaderQQNT

---
+++ fish/functions/setup-LiteLoaderQQNT.fish
@@ -0,0 +1,26 @@
+function setup-LiteLoaderQQNT -d 'Setup LiteLoaderQQNT'
+    set app_path /opt/QQ/resources/app
+    set LiteLoaderQQNT_path $app_path/LiteLoaderQQNT
+    set app_launcher_path $app_path/app_launcher
+
+    if [ -d $LiteLoaderQQNT_path ]
+        cd $LiteLoaderQQNT_path
+        git pull
+        prevd
+    else
+        git clone --depth 1 https://github.com/LiteLoaderQQNT/LiteLoaderQQNT.git
+    end
+
+    cd $app_launcher_path
+
+    if rg -qF LiteLoaderQQNT index.js
+        echo 'index.js 已经引入 LiteLoaderQQNT，跳过。'
+    else
+        echo 'require("../LiteLoaderQQNT");' >index.js.tmp
+        cat index.js >>index.js.tmp
+        sudo mv index.js.tmp index.js
+        echo 'index.js 引入 LiteLoaderQQNT 完成。'
+    end
+
+    prevd
+end
