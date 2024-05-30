da0c232b3438ff3e50da6eeefce475ad968b4da5
Bit0r <nie_wang@outlook.com>
2024-05-30 19:38:19 +0800

    feat: Add watchtower

--- configs-fish.fish
+++ configs-fish.fish
@@ -26,6 +26,9 @@ if confirm 'Do you want to install the user-level configuration for fish?'
     if type -q fnm
         fnm completions --shell fish >~/.config/fish/completions/fnm.fish
     end
+    if type -q watchtower
+        watchtower completion fish >~/.config/fish/completions/watchtower.fish
+    end
 end
 
 if confirm 'Do you want to install the system-level configuration for fish?'
