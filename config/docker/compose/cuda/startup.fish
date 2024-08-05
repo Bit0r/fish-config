4c2d1366802b98d9970cc8b6f5913ca3910411bf
Bit0r <nie_wang@outlook.com>
2024-08-06 03:15:14 +0800

    feat: Update CUDA container configuration and dependencies.

---
+++ config/docker/compose/cuda/startup.fish
@@ -0,0 +1,6 @@
+for user in dai_haohua hu_kaiyu pan_yh wu_zhiming xu_yuting zhang_jun zhao_siliang zhou_min
+    set -l uid (id -u $user)
+    # echo $user\tssh:2$uid\tvnc:5$uid
+    # sudo -u $user UID=$uid docker compose config
+    sudo -u $user UID=$uid docker compose up -d
+end
--- fish/variables/normal.fish
+++ fish/variables/normal.fish
@@ -1,3 +1,4 @@
+set -Ux UID (id -u) # 设置当前用户的 UID
 set -Ux VERSION_CONTROL numbered # 设置cp、mv等命令的备份方式，避免覆盖
 set -Ux MTR_OPTIONS -b
 set -Ux EDITOR micro
