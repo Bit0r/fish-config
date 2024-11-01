3b1a28b9539baa24468d56cdd0335398bd1eb9bd
Bit0r <nie_wang@outlook.com>
2024-11-01 20:47:18 +0800

    feat: 更新 sd-recursive 函数以优化文件搜索和替换逻辑

--- fish/functions/sd-recursive.fish
+++ fish/functions/sd-recursive.fish
@@ -1,4 +1,4 @@
 function sd-recursive -a pattern replace dir -d 'Recursively search and replace text in files within a directory'
-    set files (fd -H -x file -i '{}' $dir | rg text | tuc -d : -f 1)
+    set files (fd -H $dir -x file -i {} | rg text/ | tuc -d : -f 1)
     sd $pattern $replace $files
 end
