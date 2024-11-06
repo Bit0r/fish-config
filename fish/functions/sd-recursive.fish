3c5a0744680226331650d713406ea46aa13a283b
Bit0r <nie_wang@outlook.com>
2024-11-06 16:32:16 +0800

    feat: 优化 sd-recursive 函数以逐个文件进行文本搜索和替换

--- fish/functions/sd-recursive.fish
+++ fish/functions/sd-recursive.fish
@@ -1,4 +1,6 @@
 function sd-recursive -a pattern replace dir -d 'Recursively search and replace text in files within a directory'
     set files (fd -H $dir -x file -i {} | rg text/ | tuc -d : -f 1)
-    sd $pattern $replace $files
+    for file in $files
+        sd $pattern $replace $file
+    end
 end
