44e5cebb85f740b496ade444f6465c765398f2c5
Bit0r <nie_wang@outlook.com>
2025-08-28 10:16:16 +0800

    修改软件源镜像配置

--- config-zypper.fish
+++ config-zypper.fish
@@ -1,9 +1,19 @@
 #!/usr/bin/fish
 source include/confirm.fish
 
+# 添加镜像源
+if confirm 'Do you want to add mirror?'
+    for line in (cat ./config/zypper/mirrors.conf | grep -v '^#')
+        set line (string split ' ' $line)
+        set url $line[1]
+        set name $line[2]
+        sudo zypper ar -p 98 $url $name
+    end
+end
+
 # 添加来自 OBS (Open Build Service) 的 Zypper 软件源
 if confirm 'Do you want to add some OBS?'
-    for repo in (cat ./config/zypper/repos.txt | grep -v '^#')
+    for repo in (cat ./config/zypper/repos.conf | grep -v '^#')
         sudo zypper addrepo -p 200 $repo
     end
 end
